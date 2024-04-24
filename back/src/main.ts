33bb65fac46b962ba1eb9568b1cbd6d6fa230dc4
jolivald <jolivald.jonathan@gmail.com>
2024-04-24 14:14:34 +0200

    feat: load config from .env file

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app  = await NestFactory.create(AppModule);
+  const conf = app.get<ConfigService>(ConfigService);
+  const host = conf.get('APP_HOST');
+  const port = conf.get('APP_PORT');
+  await app.listen(port);
+  console.log(`-> Air2Java backend listening on: http://${host}:${port}`);
 }
 bootstrap();
