0de1268f57b19ce6618cbfe00114b507a91add33
jjonathan <jolivald.jonathan@gmail.com>
2024-04-28 00:26:30 +0200

    fix: disable "x-powered"-by express header

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const conf = app.get<ConfigService>(ConfigService);
   const host = conf.get('APP_HOST');
   const port = conf.get('APP_PORT');
+  app.disable('x-powered-by');
   await app.listen(port);
   console.log(`-> Air2Java backend listening on: http://${host}:${port}`);
 }
