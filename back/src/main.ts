5458b252938d0d467cd78dec1b1c436839eba93f
jolivald <jolivald.jonathan@gmail.com>
2024-05-15 16:58:26 +0200

    fix: swagger metadata

--- back/src/main.ts
+++ back/src/main.ts
@@ -30,10 +30,9 @@ async function bootstrap() {
   // app.useGlobalInterceptors(new TransformInterceptor());
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Air de Java')
+    .setDescription('API documentation')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
