5204361da15770175296517bbaaaf5e58c063148
jolivald <jolivald.jonathan@gmail.com>
2024-05-22 11:18:21 +0200

    wip: prepare for CI/CD pipeline

--- back/src/authentication/jwt.strategy.ts
+++ back/src/authentication/jwt.strategy.ts
@@ -3,7 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request } from 'express';
-import { AppuserService } from 'src/appuser/appuser.service';
+import { AppuserService } from '../appuser/appuser.service';
 import { TokenPayload } from './tokenPayload.interface';
 
 @Injectable()
